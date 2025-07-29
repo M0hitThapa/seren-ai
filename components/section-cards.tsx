939514cdbe88f706d9c11414592bbae4873c2a03
Mohit Thapa <thapamohit2058@gmail.com>
2025-07-29 09:03:14 +0545

    add sectioncards for showing details overview

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -1,4 +1,4 @@
-import { TrendingDownIcon, TrendingUpIcon } from "lucide-react"
+import { Brain, ChartBar, Heart, LineChart, LucideMove, TrendingDownIcon, TrendingUpIcon, Trophy } from "lucide-react"
 
 import { Badge } from '@/components/ui/badge'
 import {
@@ -12,90 +12,83 @@ import {
 export function SectionCards() {
   return (
     <div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6">
-      <Card className="@container/card">
+      <Card className="@container/card bg-teal-100">
         <CardHeader className="relative">
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            $1,250.00
-          </CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +12.5%
-            </Badge>
+          <div className="flex gap-2 items-center">
+            <Brain />
+          <CardDescription>Mood Score</CardDescription>
           </div>
+          
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            No Data
+          </CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <TrendingUpIcon className="size-4" />
+            Todays Average mood
           </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
+          
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className="@container/card bg-blue-100">
         <CardHeader className="relative">
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            1,234
-          </CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingDownIcon className="size-3" />
-              -20%
-            </Badge>
+          <div className="flex gap-2 items-center">
+            <Trophy />
+          <CardDescription>Completion Rate</CardDescription>
           </div>
+          
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            0%
+          </CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <TrendingDownIcon className="size-4" />
+            Activities completed
           </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
+          
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className="@container/card bg-purple-100">
         <CardHeader className="relative">
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            45,678
-          </CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +12.5%
-            </Badge>
+          <div className="flex gap-2 items-center">
+            <Heart />
+          <CardDescription>MindFulness</CardDescription>
           </div>
+          
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            0 sessions
+          </CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <TrendingUpIcon className="size-4" />
+            Mindfulness activities
           </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
+          
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className="@container/card bg-rose-100">
         <CardHeader className="relative">
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            4.5%
-          </CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +4.5%
-            </Badge>
+          <div className="flex gap-2 items-center">
+            <LineChart />
+          <CardDescription>Total Activities</CardDescription>
           </div>
+          
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            0
+          </CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance <TrendingUpIcon className="size-4" />
+            Planned for today
           </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+          
         </CardFooter>
       </Card>
+      
     </div>
   )
 }
